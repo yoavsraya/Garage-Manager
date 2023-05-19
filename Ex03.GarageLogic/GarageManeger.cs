a0de108ff62d11c2b3a946c5a1cb9d50dae16b6e
Jonathan Brooker <jonathanbr@mta.ac.il>
2023-05-19 15:31:16 +0300

    big step clientInfo+Garage

--- Ex03.GarageLogic/GarageManeger.cs
+++ Ex03.GarageLogic/GarageManeger.cs
@@ -1,31 +1,71 @@
 ﻿using System.Collections.Generic;
-
+using System;
 
 namespace Ex03.GarageLogic
 {
     class GarageManeger
     {
         private Dictionary<ClientInfo.eClientStatus, ClientInfo> m_clients;
-        public void EnteringNewVehicle(in string i_licensePlate)
+        
+        public List<string> ReturnListOfPlatesByFilter(in string i_condition)
         {
+            List<string> filteredPlates = new List<string>();
 
+            if (Enum.TryParse(i_condition, out ClientInfo.eClientStatus clientStatus) == false)
+            {
+                throw new Exception();
+            }
+
+            foreach (KeyValuePair<ClientInfo.eClientStatus, ClientInfo> vehicle in m_clients)
+            {
+                if (vehicle.Key == clientStatus)
+                {
+                    filteredPlates.Add(vehicle.Value.getViheclePlateNumber());
+                }
+            }
+
+            return filteredPlates;
         }
 
-        public List<string> ReturnListOfPlatesByFilter()
+        public bool UpdateClientStatus(in string i_PlateNumber, in string i_NewStatus)
         {
-            List<string> i = new List<string>();
+            const bool v_Found = true;
+            bool isPlateFound = !v_Found;
 
-            return i;
+            if(Enum.TryParse(i_NewStatus, out ClientInfo.eClientStatus newClientStatus) == false) 
+            {
+                throw new Exception();
+            }
+            
+            foreach (KeyValuePair<ClientInfo.eClientStatus, ClientInfo> vehicle in m_clients)
+            {
+                if (i_PlateNumber.Equals(vehicle.Value.getViheclePlateNumber())) 
+                {
+                    isPlateFound = v_Found;
+                    vehicle.Value.clientStatus = newClientStatus;
+                    break;
+                }
+            }
+
+            return isPlateFound;
         }
 
-        public void UpdateClientStatus(in string i_PlateNumber, in string i_NewStatus)
+        public bool FillWheelsToMax(string i_PlateNumber)
         {
+            const bool v_Found = true;
+            bool isPlateFound = !v_Found;
 
-        }
+            foreach(KeyValuePair<ClientInfo.eClientStatus, ClientInfo> client in m_clients) 
+            {
+                if (client.Value.getViheclePlateNumber().Equals(i_PlateNumber)) 
+                {
+                    isPlateFound = true;
+                    client.Value.FillWheelsAirToMax();
+                    break;
+                }
+            }
 
-        public void FillWheelsToMax(string i_PlateNumber)
-        {
-
+            return isPlateFound;
         }
 
 
