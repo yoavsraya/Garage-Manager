ac049aad4f8e1456dcafec43b7db6dbf59665afd
Yoav Sraya <yoavsr@mta.ac.il>
2023-05-24 17:47:10 +0300

    new value exception

---
+++ Ex03.GarageLogic/ValueOutOfRangeException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex03.GarageLogic
+{
+    class ValueOutOfRangeException :Exception
+    {
+        public override string Message
+        {
+            get
+            {
+                return "value is out of Range!";
+            }
+        }
+
+        private float m_MaxValue { get; set; }
+        private float m_MinValue { get; set; }
+    }
+}
